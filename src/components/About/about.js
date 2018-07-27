2668c2534ab9e93dbdd66bbac6c5ff271fc6d5ce
allisa liberto <allisa84@yahoo.com>
2018-07-27 13:42:48 -0500

    fixed spelling errors

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -10,7 +10,7 @@ export default class About extends Component {
           </div>
           <div className="nine columns main-col">
             <h2>About Me</h2>
-            <p>I am an ambitious, creative, and resourceful full stack web developer who is driven to continuously learn new technologies and creative skills. I have a mutlifaceted background in customer service, photography, and entrepreneurship that has given me a strong ability to communicate effectively and multitask like a master. I've just recently completed an intensive 24 week coding bootcamp in which I gained more inspiration and knowledge than my brain thought possible.
+            <p>I am an ambitious, creative, and resourceful full stack web developer who is driven to continuously learn new technologies and creative skills. I have a multifaceted background in customer service, photography, and entrepreneurship that has given me a strong ability to communicate effectively and multitask like a master. I've just recently completed an intensive 24 week coding bootcamp in which I gained more inspiration and knowledge than my brain thought possible.
             </p>
             <p>Beyond technology I am a mother to a feisty daughter, a foodie, a music lover and a treehugger. In my free time you might find me hiking, reading, learning new languages or expanding on previous knowledge, as well as exploring this wide world we all call home.</p>
             <div className="row">
